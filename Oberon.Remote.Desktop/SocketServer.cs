820eec5d81d6f75162464376b0f3cba2eab85255
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-03-05 08:13:56 +0300

    Chore: Credit GetListenIP()
    Co-Authored-By: SamsidParty <business@samsidparty.com>

--- Oberon.Remote.Desktop/SocketServer.cs
+++ Oberon.Remote.Desktop/SocketServer.cs
@@ -37,6 +37,7 @@ namespace Oberon.Remote.Desktop
             }
         }
 
+        // https://stackoverflow.com/a/27376368
         public static string GetListenIP()
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
