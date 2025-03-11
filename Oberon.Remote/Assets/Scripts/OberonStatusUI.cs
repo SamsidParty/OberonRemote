3413378a84024a0d7c8f2fe52cc4ceb52b26904e
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-03-11 17:17:52 +0300

    Add controller preview on unity remote
    Co-Authored-By: SamsidParty <business@samsidparty.com>

--- Oberon.Remote/Assets/Scripts/OberonStatusUI.cs
+++ Oberon.Remote/Assets/Scripts/OberonStatusUI.cs
@@ -1,14 +1,60 @@
 using Oberon.Remote.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class OberonStatusUI : MonoBehaviour
 {
     public Text IPText;
+    public Text ControllerText;
+
+    public ControllerPreview[] ControllerTextures;
+    public Image ControllerImage;
+
+    string CurrentControllerImage;
+    string LastControllerImage = "Missing";
 
     private void FixedUpdate()
     {
         var status = ServerStatus.CurrentStatus;
         IPText.text = "IP Address: " + status.ListenIP;
+
+        if (status.ConnectedControllerCount == 0)
+        {
+            ControllerText.text = "Controller: Disconnected";
+            CurrentControllerImage = "Missing";
+        }
+        else if (status.ConnectedControllerCount == 1)
+        {
+            ControllerText.text = "Controller: Connected";
+
+            CurrentControllerImage = "Generic";
+
+            if (Gamepad.current.displayName.ToLower().Contains("dualsense"))
+            {
+                CurrentControllerImage = "PS5";
+            }
+        }
+        else
+        {
+            ControllerText.text = "Controllers: " + status.ConnectedControllerCount + " Connected";
+            CurrentControllerImage = "Generic";
+        }
+
+        if (CurrentControllerImage != LastControllerImage)
+        {
+            LastControllerImage = CurrentControllerImage;
+            ControllerImage.sprite = ControllerTextures.Where((t) => t.ControllerType == CurrentControllerImage).First().Texture;
+        }
     }
+}
+
+[Serializable]
+public struct ControllerPreview
+{
+    public string ControllerType;
+    public Sprite Texture;
 }
