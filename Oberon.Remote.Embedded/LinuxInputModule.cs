3763f647a74ec1fe10d33b13a0d4add277ec916d
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-03-21 17:22:30 +0300

    Start work on embedded remote

---
+++ Oberon.Remote.Embedded/LinuxInputModule.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oberon.Remote.Core;
+
+namespace Oberon.Remote.Embedded
+{
+    public class LinuxInputModule : InputModule
+    {
+        public void StartProcessingInputs() {
+            
+        }
+    }
+}
---
+++ Oberon.Remote.Embedded/Program.cs
@@ -0,0 +1,22 @@
+﻿using Oberon.Remote.Core;
+
+namespace Oberon.Remote.Embedded {
+    public class Program {
+        public static void Main(string[] args) {
+
+            ServerStatus.CurrentStatus.MachineName = "[Embedded Remote] " + Environment.MachineName;
+            OberonManager.Initialize(new LinuxInputModule());
+
+            new Thread(() => (OberonManager.InputModule as LinuxInputModule).StartProcessingInputs()).Start();
+
+            Console.WriteLine("Started Input Server");
+            Console.WriteLine("Listening On IP Address: " + SocketServer.GetListenIP());
+            Console.WriteLine("Press Any Key To Stop");
+            
+            Console.ReadKey();
+
+            Console.WriteLine("Stopping...");
+            OberonManager.SocketServer.Stop();
+        }
+    }
+}
