73cbd1eacb4b9b0978eaf021b614b53bac5e8351
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-06-10 10:32:39 +0300

    Remote: Refactor InsertInputBytes to support arrow key navigation
    Co-Authored-By: SamsidParty <business@samsidparty.com>

--- Oberon.Remote/Assets/Scripts/UnityInputModule.cs
+++ Oberon.Remote/Assets/Scripts/UnityInputModule.cs
@@ -6,30 +6,41 @@ using UnityEngine.InputSystem;
 public class UnityInputModule : InputModule
 {
 
-    void InsertInputBytes(byte[] bytes, int offset, Gamepad gamepad)
+    void InsertInputBytes(byte[] bytes, int offset, Gamepad gamepad, Keyboard keyboard)
     {
         bytes[offset] = 0xFF;
-        InsertUInt16(bytes, 1 + offset, Mathf.FloorToInt(gamepad.leftStick.x.value * 32767));
-        InsertUInt16(bytes, 3 + offset, Mathf.FloorToInt(gamepad.leftStick.y.value * -32767));
-        InsertUInt16(bytes, 5 + offset, Mathf.FloorToInt(gamepad.rightStick.x.value * 32767));
-        InsertUInt16(bytes, 7 + offset, Mathf.FloorToInt(gamepad.rightStick.y.value * -32767));
-        InsertUInt16(bytes, 9 + offset, Mathf.FloorToInt(gamepad.leftTrigger.value * 32767));
-        InsertUInt16(bytes, 11 + offset, Mathf.FloorToInt(gamepad.rightTrigger.value * 32767));
+        InsertUInt16(bytes, 1 + offset, Mathf.FloorToInt((gamepad?.leftStick.x.value ?? 0) * 32767));
+        InsertUInt16(bytes, 3 + offset, Mathf.FloorToInt((gamepad?.leftStick.y.value ?? 0) * -32767));
+        InsertUInt16(bytes, 5 + offset, Mathf.FloorToInt((gamepad?.rightStick.x.value ?? 0) * 32767));
+        InsertUInt16(bytes, 7 + offset, Mathf.FloorToInt((gamepad?.rightStick.y.value ?? 0) * -32767));
+        InsertUInt16(bytes, 9 + offset, Mathf.FloorToInt((gamepad?.leftTrigger.value ?? 0) * 32767));
+        InsertUInt16(bytes, 11 + offset, Mathf.FloorToInt((gamepad?.rightTrigger.value ?? 0) * 32767));
 
-        var buttonGroup1 = new bool[] { gamepad.buttonSouth.isPressed, gamepad.buttonEast.isPressed, gamepad.buttonWest.isPressed, gamepad.buttonNorth.isPressed, gamepad.leftShoulder.isPressed, gamepad.rightShoulder.isPressed, gamepad.startButton.isPressed, gamepad.selectButton.isPressed };
-        var buttonGroup2 = new bool[] { false, gamepad.leftStickButton.isPressed, gamepad.rightStickButton.isPressed, gamepad.dpad.up.isPressed, gamepad.dpad.down.isPressed, gamepad.dpad.left.isPressed, gamepad.dpad.right.isPressed, false };
+        var buttonGroup1 = new bool?[] { 
+            gamepad?.buttonSouth.isPressed | keyboard?.enterKey.isPressed,
+            gamepad?.buttonEast.isPressed | keyboard?.escapeKey.isPressed,
+            gamepad?.buttonWest.isPressed,
+            gamepad?.buttonNorth.isPressed,
+            gamepad?.leftShoulder.isPressed,
+            gamepad?.rightShoulder.isPressed,
+            gamepad?.startButton.isPressed,
+            gamepad?.selectButton.isPressed
+        };
+        var buttonGroup2 = new bool?[] { 
+            false,
+            gamepad?.leftStickButton.isPressed,
+            gamepad?.rightStickButton.isPressed,
+            gamepad?.dpad.up.isPressed | keyboard?.upArrowKey.isPressed,
+            gamepad?.dpad.down.isPressed | keyboard?.downArrowKey.isPressed,
+            gamepad?.dpad.left.isPressed | keyboard?.leftArrowKey.isPressed,
+            gamepad?.dpad.right.isPressed | keyboard?.rightArrowKey.isPressed,
+            keyboard?.ctrlKey.isPressed
+        };
 
         bytes[13 + offset] = GenerateButtonGroup(buttonGroup1);
         bytes[14 + offset] = GenerateButtonGroup(buttonGroup2);
     }
 
-    void InsertNullInputBytes(byte[] bytes, int offset)
-    {
-        for (int i = 0; i < 24; i++)
-        {
-            bytes[offset + i] = 0;
-        }
-    }
 
     public void ProcessInput()
     {
@@ -37,14 +48,11 @@ public class UnityInputModule : InputModule
 
         for (int i = 0; i < 4; i++)
         {
-            if (i >= Gamepad.all.Count || Gamepad.all[i] == null)
-            {
-                InsertNullInputBytes(bytes, i * 25);
-                continue;
-            }
+            // Try to get the gamepad at index i or null
+            var gamepad = (i >= Gamepad.all.Count) ? null : Gamepad.all[i];
 
-            Gamepad.all[i].SetMotorSpeeds(RumbleValues[i].LeftMotor / 255f, RumbleValues[i].RightMotor / 255f);
-            InsertInputBytes(bytes, i * 25, Gamepad.all[i]);
+            gamepad?.SetMotorSpeeds(RumbleValues[i].LeftMotor / 255f, RumbleValues[i].RightMotor / 255f);
+            InsertInputBytes(bytes, i * 25, gamepad, i == 0 ? Keyboard.current : null);
         }
 
         bytes[0] = 0xFF;
@@ -59,14 +67,14 @@ public class UnityInputModule : InputModule
 
     #region Helpers
 
-    public static byte GenerateButtonGroup(bool[] source)
+    public static byte GenerateButtonGroup(bool?[] source)
     {
         byte result = 0;
         int index = 8 - source.Length;
 
-        foreach (bool b in source)
+        foreach (bool? b in source)
         {
-            if (b)
+            if (b ?? false)
                 result |= (byte)(1 << (7 - index));
 
             index++;
